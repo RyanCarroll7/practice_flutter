e2ee3b1efa1b01cf76dc7f14626bcc16f4701672
Ryan Carroll <ryan.carroll421@gmail.com>
2025-01-28 17:40:22 -0500

    Column practice

--- lib/main.dart
+++ lib/main.dart
@@ -10,27 +10,37 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Colors.teal,
-      appBar: AppBar(
-        title: Text('Container Widget'),
-        backgroundColor: Colors.red,
-        elevation: 0,
-        leading: Icon(Icons.menu),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
-      ),
-      body: Center(
-        child: Container(
-          height: 300,
-          width: 300,
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(50),
-          ),
-          padding: EdgeInsets.all(25),
-          child: Icon(Icons.favorite, color: Colors.red, size: 100),
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          title: Text('Container Widget'),
+          backgroundColor: Colors.red,
+          elevation: 0,
+          leading: Icon(Icons.menu),
+          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.white,
+            ),
+            Container(
+              width: 300,
+              height: 100,
+              color: const Color.fromARGB(255, 104, 39, 39),
+            ),
+            Container(
+              width: 50,
+              height: 100,
+              color: const Color.fromARGB(255, 255, 20, 20),
+            ),
+          ],
         ),
       ),
-    ));
+    );
   }
 }
